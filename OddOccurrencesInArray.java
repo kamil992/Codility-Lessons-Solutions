17f5e5dcd36cf4233e6ee9b1eee57146df063cf6
Kamil Biernacki <kbiernacki67@gmail.com>
2018-06-04 12:53:53 +0200

    codility

---
+++ OddOccurrencesInArray.java
@@ -0,0 +1,20 @@
+package Codility;
+
+import java.util.Arrays;
+
+public class OddOccurrencesInArray {
+    public static void main(String[] args) {
+        int[] a = {9,3,9,3,9,7,9};
+        System.out.println(solution(a));
+        
+    }
+
+    public static int solution(int[] A){
+        int num = 0;
+
+        for(int i = 0; i < A.length; i++){
+            num = num ^ A[i];
+        }
+        return num;
+    }
+}
